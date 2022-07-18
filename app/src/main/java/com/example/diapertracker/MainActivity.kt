1e64de7e3203ec9a424ff4483de4e79da10a0352
shawnee <shanpan513@gmail.com>
2022-07-18 13:58:18 -0600

    first commit

---
+++ app/src/main/java/com/example/diapertracker/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.diapertracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
